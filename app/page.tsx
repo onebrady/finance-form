d3a16af450e089a93b878c72efda393c3f073abd
Brady Freeman <onebrady@gmail.com>
2024-02-03 23:58:58 -0600

    form page update 5

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col bg-white items-center justify-start p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <div className="pb-5">
         <Image
           className="relative"
